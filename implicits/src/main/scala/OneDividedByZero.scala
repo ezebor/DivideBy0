eece56df0088080fe0b4f60682580f42519d3ad2
Ezequiel Borenstein <ezequielborenstein@MacBook-Pro.fibertel.com.ar>
2022-07-27 11:04:10 -0300

    Fixed infinite list

--- implicits/src/main/scala/OneDividedByZero.scala
+++ implicits/src/main/scala/OneDividedByZero.scala
@@ -3,22 +3,23 @@ object OneDividedByZero extends App {
   case class Operand(number: Double) {
 
     def /~(divisor: Operand): Double = {
-      def infiniteList(auxDivisor: Double): LazyList[Double] = {
+      def betweenLimits(number: Double): Double = Math.min(Math.max(number, Double.MinValue), Double.MaxValue)
+
+      def infiniteList(auxDivisor: Double): LazyList[(Double, Double)] = {
         val average = (auxDivisor + divisor.number) / 2
-        val newResult = Math.min(Math.max(number / average, Double.MinValue), Double.MaxValue)
-        newResult #:: infiniteList(average)
+        (betweenLimits(number / auxDivisor), betweenLimits(number / average)) #:: infiniteList(average)
       }
 
-      def converge(results: LazyList[Double]): Double = results match {
-        case result1 #:: result2 #:: othersResults =>
-          if(Math.abs(result2 - result1) <= 1E-10) result2
-          else converge(result2 #:: othersResults)
-      }
+      def converge(results: LazyList[(Double, Double)]): Double = results
+        .filter { case (result1, result2) => Math.abs(result2 - result1) <= 1E-10}
+        .head
+        ._2
 
       converge(infiniteList(number))
     }
   }
 
   implicit def toNumber(n: Double): Operand = Operand(n)
-  println(1.0 /~ 2.0)
+
+  println(1.0 /~ 0.0)
 }
