3acba2f255f6e7b060bb36a70457a24aa54616fa
Ezequiel Borenstein <ezequiel.borenstein@gmail.com>
2022-08-20 13:59:10 -0300

    Refactors of algorithm

--- implicits/src/main/scala/OneDividedByZero.scala
+++ implicits/src/main/scala/OneDividedByZero.scala
@@ -27,5 +27,62 @@ object OneDividedByZero extends App {
 
   implicit def toNumber(n: Double): Operand = Operand(n)
 
-  println(1.0 /~ 0.0)
+  //println(1.0 /~ 0.0)
+
+  /*
+  1- Crear una sucesión de divisores candidatos que converja al divisor que necesitamos
+  2- Crear una sucesión de cocientes, efectuando la división entre el dividendo y cada uno de los divisores candidatos
+  3- Hallar la convergencia en la sucesión de cocientes
+  4- Hallar el cociente target a partir del cual se considera convergente a la sucesión
+   */
+
+  case class Division(dividend: Double, divisor: Double) {
+    /*def limit(number: Double): Double = Math.min(Math.max(number, Double.MinValue), Double.MaxValue)
+
+    def foo = {
+      lazy val infiniteResults: LazyList[(Double, Boolean)] = LazyList.iterate(((dividend + divisor) / 2, false)){
+        case (currentDivisor, _) =>
+          val newDivisor = (currentDivisor + divisor) / 2
+          val currentResult = limit(dividend / currentDivisor)
+          val newResult = limit(dividend / newDivisor)
+          val newHasConverded = Math.abs(newResult - currentResult) <= 1E-10
+          (newDivisor, newHasConverded)
+      }
+
+      val finalDivisor = infiniteResults
+        .dropWhile{case (_, hasConverged) => !hasConverged}
+        .head
+        ._1
+
+      limit(dividend / finalDivisor)
+    }
+
+    def divide() = {
+
+    }*/
+
+    def /~(number1: Double, number2: Double) = Math.min(Math.max(number1 / number2, Double.MinValue), Double.MaxValue)
+
+    def divide() = {
+      // step 1
+      lazy val divisorsCandidates = LazyList.iterate(dividend)(divisorCandidate => (divisorCandidate + divisor) /~ 2)
+      // step 2
+      lazy val quotientsCandidates = divisorsCandidates.map(divisorCandidate => dividend /~ divisorCandidate)
+      // step 3
+      var quotientTarget = quotientsCandidates.head
+      val convergence = quotientsCandidates.tail.map { currentQuotient =>
+        val convergencePoint = (currentQuotient, Math.abs(quotientTarget - currentQuotient) <= 1E-10)
+        quotientTarget = currentQuotient
+        convergencePoint
+      }
+      // step 4
+      convergence
+        .dropWhile{case (_, hasConverged) => !hasConverged}
+        .head
+        ._1
+    }
+
+  }
+
+  println(Division(1.0, 0.0).divide())
 }
